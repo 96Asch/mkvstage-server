b17021757b196638d9e05b01aeaab1c4f4dff05c
96Asch <96Asch@github.com>
2023-02-16 20:58:15 +0100

    Add song repo functions

--- internal/repository/songrepo.go
+++ internal/repository/songrepo.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/96Asch/mkvstage-server/internal/domain"
 	"gorm.io/gorm"
@@ -18,21 +20,59 @@ func NewGormSongRepository(db *gorm.DB) *gormSongRepository {
 }
 
 func (sr gormSongRepository) GetByID(ctx context.Context, sid int64) (*domain.Song, error) {
-	return nil, nil
+	var song domain.Song
+	res := sr.db.First(&song, sid)
+
+	if err := res.Error; err != nil {
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			return nil, domain.NewRecordNotFoundErr("id", fmt.Sprint(sid))
+		default:
+			return nil, domain.NewInternalErr()
+		}
+	}
+
+	return &song, nil
 }
 
 func (sr gormSongRepository) GetAll(ctx context.Context) (*[]domain.Song, error) {
-	return nil, nil
+	var songs []domain.Song
+	res := sr.db.Find(&songs)
+
+	if err := res.Error; err != nil {
+		return nil, domain.NewInternalErr()
+	}
+
+	return &songs, nil
 }
 
 func (sr gormSongRepository) Create(ctx context.Context, song *domain.Song) error {
+	res := sr.db.Create(song)
+
+	if err := res.Error; err != nil {
+		return domain.NewInternalErr()
+	}
+
 	return nil
 }
 
 func (sr gormSongRepository) Delete(ctx context.Context, sid int64) error {
+	song := domain.Song{ID: sid}
+	res := sr.db.Delete(&song)
+
+	if err := res.Error; err != nil {
+		return domain.NewInternalErr()
+	}
+
 	return nil
 }
 
 func (sr gormSongRepository) Update(ctx context.Context, song *domain.Song) error {
+	res := sr.db.Save(song)
+
+	if err := res.Error; err != nil {
+		return domain.NewInternalErr()
+	}
+
 	return nil
 }

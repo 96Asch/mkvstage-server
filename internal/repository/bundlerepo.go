646b1d7ce70f9be1508d389276c2b723d90fe9d1
96Asch <96Asch@github.com>
2023-02-16 15:08:39 +0100

    Add implementations for bundlerepository

--- internal/repository/bundlerepo.go
+++ internal/repository/bundlerepo.go
@@ -2,33 +2,70 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/96Asch/mkvstage-server/internal/domain"
 	"gorm.io/gorm"
 )
 
 type gormBundleRepository struct {
-	DB *gorm.DB
+	db *gorm.DB
 }
 
 func NewGormBundleRepository(db *gorm.DB) *gormBundleRepository {
 	return &gormBundleRepository{
-		DB: db,
+		db: db,
 	}
 }
 
 func (br gormBundleRepository) GetByID(ctx context.Context, bid int64) (*domain.Bundle, error) {
-	return nil, nil
+	var bundle domain.Bundle
+	res := br.db.First(&bundle, bid)
+	if err := res.Error; err != nil {
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			return nil, domain.NewRecordNotFoundErr("id", fmt.Sprint(bid))
+		default:
+			return nil, domain.NewInternalErr()
+		}
+	}
+
+	return &bundle, nil
 }
 func (br gormBundleRepository) GetAll(ctx context.Context) (*[]domain.Bundle, error) {
-	return nil, nil
+	var bundles []domain.Bundle
+	res := br.db.Find(&bundles)
+	if err := res.Error; err != nil {
+		return nil, domain.NewInternalErr()
+	}
+
+	return &bundles, nil
 }
+
 func (br gormBundleRepository) Create(ctx context.Context, bundle *domain.Bundle) error {
+	res := br.db.Create(bundle)
+	if err := res.Error; err != nil {
+		return domain.NewInternalErr()
+	}
+
 	return nil
 }
+
 func (br gormBundleRepository) Delete(ctx context.Context, bid int64) error {
+	bundle := domain.Bundle{ID: bid}
+	res := br.db.Delete(&bundle)
+	if err := res.Error; err != nil {
+		return domain.NewInternalErr()
+	}
+
 	return nil
 }
 func (br gormBundleRepository) Update(ctx context.Context, bundle *domain.Bundle) error {
+	res := br.db.Save(bundle)
+	if err := res.Error; err != nil {
+		return domain.NewInternalErr()
+	}
+
 	return nil
 }

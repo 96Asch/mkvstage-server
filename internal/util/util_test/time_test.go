1e36ce3344bb4adad53e38b47dbd66f49c2decfd
96Asch <96Asch@github.com>
2023-07-21 12:19:03 +0200

    Remove StringToDate + Tests

--- internal/util/util_test/time_test.go
+++ internal/util/util_test/time_test.go
@@ -6,36 +6,7 @@ import (
 
 	"github.com/96Asch/mkvstage-server/internal/util"
 	"github.com/stretchr/testify/assert"
-	"gorm.io/datatypes"
 )
-
-func TestStringToDate(t *testing.T) {
-	t.Parallel()
-
-	expecteds := map[string]datatypes.Date{
-		"1996-02-08": datatypes.Date(time.Date(1996, 2, 8, 0, 0, 0, 0, time.UTC)),
-		"2023-05-13": datatypes.Date(time.Date(2023, 5, 13, 0, 0, 0, 0, time.UTC)),
-		"2000-01-01": datatypes.Date(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)),
-	}
-
-	for key, val := range expecteds {
-		date, err := util.StringToDate(key)
-		assert.NoError(t, err)
-		assert.Equal(t, val, date)
-	}
-
-	fails := map[string]datatypes.Date{
-		"foobar":       {},
-		"2023--13":     {},
-		"2000-0132-01": {},
-	}
-
-	for key, val := range fails {
-		date, err := util.StringToDate(key)
-		assert.Error(t, err)
-		assert.Equal(t, val, date)
-	}
-}
 
 func TestStringToTime(t *testing.T) {
 	t.Parallel()
